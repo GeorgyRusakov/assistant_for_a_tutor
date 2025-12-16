43dae5bf8256d32ad2fdf0b4812086b36debb880
Georgy R <rusakov.georgy@gmail.com>
2025-12-16 16:33:20 +0300

    Добавление основной инфраструктуры бота

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
-print('Hello world')
+import asyncio
+import logging
+
+from config.config import Config, load_config
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from app.bot.handlers.handlers import user_router, start_dialog
+from aiogram_dialog import setup_dialogs
+from app.locale.ru import RU
+from app.bot.middelwares.db_middleware import DataBaseMiddleware
+from app.infrastructure.database.connection import get_pg_pool
+import psycopg_pool
+from redis.asyncio import Redis
+
+logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
+    config: Config = load_config()
+
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+
+    logger.info('Starting bot')
+
+    storage = RedisStorage(
+        redis=Redis(
+            host=config.redis.host,
+            port=config.redis.port,
+            db=config.redis.db,
+            password=config.redis.password,
+            username=config.redis.username,
+        )
+    )
+
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    dp = Dispatcher(storage=storage)
+
+    db_pool: psycopg_pool.AsyncConnectionPool = await get_pg_pool(
+        db_name=config.db.name,
+        host=config.db.host,
+        port=config.db.port,
+        user=config.db.user,
+        password=config.db.password,
+    )
+    logger.info("Including routers...")
+    dp.include_routers(user_router)
+    dp.include_routers(start_dialog)
+    setup_dialogs(dp)
+
+    logger.info("Including middlewares...")
+    dp.update.middleware(DataBaseMiddleware())
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    try:
+        await dp.start_polling(bot, local=RU,
+                               db_pool=db_pool)
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        await db_pool.close()
+        logger.info("Connection to Postgres closed")
+
+
+asyncio.run(main())
---
+++ migrations/create_tables.py
@@ -0,0 +1,85 @@
+import asyncio
+import logging
+import os
+import sys
+
+from app.infrastructure.database.connection import get_pg_connection
+from config.config import Config, load_config
+from psycopg import AsyncConnection, Error
+
+config: Config = load_config()
+
+logging.basicConfig(
+    level=logging.getLevelName(level=config.log.level),
+    format=config.log.format,
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    connection: AsyncConnection | None = None
+
+    try:
+        connection = await get_pg_connection(
+            db_name=config.db.name,
+            host=config.db.host,
+            port=config.db.port,
+            user=config.db.user,
+            password=config.db.password,
+        )
+        async with connection:
+            async with connection.transaction():
+                async with connection.cursor() as cursor:
+                    await cursor.execute(
+                        query="""
+                            CREATE TABLE IF NOT EXISTS students(
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(30) NOT NULL,
+                            grade INT NOT NULL
+                            );
+                            """
+                    )
+                    await cursor.execute(
+                        query="""
+                        CREATE TABLE IF NOT EXISTS subject(
+                        id SERIAL PRIMARY KEY,
+                        item_name VARCHAR(20) NOT NULL
+                        );
+                        """
+                    )
+                    await cursor.execute(
+                        query="""
+                        CREATE TABLE IF NOT EXISTS classes
+                        id SERIAL PRIMARY KEY,
+                        id_student INT NOT NULL REFERENCES students(id),
+                        id_subject INT NOT NULL REFERENCES subject(id),
+                        class_date DATE NOT NULL DEFAULT CURRENT_DATE,
+                        price INT NOT NULL REFERENCES timetable(price)
+                        );
+                        """
+                    )
+                    await cursor.execute(
+                        query="""
+                        CREATE TABLE IF NOT EXISTS timetable
+                        id SERIAL PRIMARY KEY,
+                        id_student INT NOT NULL REFERENCES students(id),
+                        id_subject INT NOT NULL REFERENCES subject(id),
+                        day_week VARCHAR(20) NOT NULL,
+                        time TIMESTAMPTZ NOT NULL,
+                        price INT NOT NUll
+                        );
+                        """
+                    )
+                logger.info("Tables `students`, `subject`, classes`, `timetable` were successfully created")
+    except Error as db_error:
+        logger.exception("Database-specific error: %s", db_error)
+    except Exception as e:
+        logger.exception("Unhandled error: %s", e)
+    finally:
+        if connection:
+            await connection.close()
+            logger.info("Connection to Postgres closed")
+
+
+asyncio.run(main())
